92467571d52989823ebad77e097b6942d32d1ad2
AdamBtech <60339324+AdamBtech@users.noreply.github.com>
2025-05-21 17:52:09 +0200

    Operational dark mode button added

--- src/app/features/header-display/header-contact-links/header-contact-links.component.ts
+++ src/app/features/header-display/header-contact-links/header-contact-links.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import {HeaderSwitchThemeButtonComponent} from './header-switch-theme-button/header-switch-theme-button.component';
 
 @Component({
   selector: 'app-header-contact-links',
-  imports: [],
+  imports: [HeaderSwitchThemeButtonComponent],
   templateUrl: './header-contact-links.component.html',
-  styleUrl: './header-contact-links.component.css'
+  styleUrl: './header-contact-links.component.css',
 })
-export class HeaderContactLinksComponent {
-
-}
+export class HeaderContactLinksComponent {}
